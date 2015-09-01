1c63aa8e4763f92fb3fc11020f2fd7232d81543b
Trent Newton <trent99878@gmail.com>
2015-09-01 21:19:14 +1000

    new update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,105 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// background animation
+
+$(document).ready(function(){
+	// set #background-* to full window height and fade in the body
+	var width = $(window).width();
+	var height = $(window).height();
+		$('#bg-container, #background-1, #background-2').css({
+			'min-width': width,
+			'min-height': height
+		});
+
+	// call new svg and start recreate svg timeout
+	svgNew();
+	recreateSvg();
+});
+
+// set global svg object
+var svg = {};
+// used to determine which background to draw to
+var draw = 1;
+// create new svg 
+var svgNew = function(){
+	svg.t = new Trianglify({
+		noiseIntensity: 0,
+	});
+	// set svg size to window height and width
+	svg.width = $(window).width();
+	svg.height = $(window).height();
+	svg.pattern = svg.t.generate(svg.width, svg.height);
+	// draw svg on to either background 1 or 2
+	if (draw === 1) {
+		svgDraw1();
+	} else {
+		svgDraw2();
+	}
+}; // end svgNew
+
+// draw svg on to bg1 and call fade
+// if called with resize, redraw the svg to match new size and do not call fade
+var svgDraw1 = function (resize){
+	draw = 2;
+	if (resize === 'resize') {	
+		svg.pattern = svg.t.generate(svg.width, svg.height);
+		$('#background-1').css({
+			'min-width': svg.width,
+			'min-height': svg.height,
+			'background': svg.pattern.dataUrl
+		});
+	} else {
+		$('.background-1').css({
+			'background': svg.pattern.dataUrl
+		});
+		fade1();
+	}
+}; // end svgDraw1
+
+// same as above but for bg2
+var svgDraw2 = function(resize){
+	draw = 1;
+	if (resize === 'resize') {	
+		svg.pattern = svg.t.generate(svg.width, svg.height);
+		$('#background-2').css({
+			'min-width': svg.width,
+			'min-height': svg.height,
+			'background': svg.pattern.dataUrl
+		});
+	} else {
+		$('.background-2').css({
+			'background': svg.pattern.dataUrl
+		});
+		fade2();
+	}
+}; // end svgDraw2
+
+// fade in bg1 and fade our bg2
+var fade1 = function(){
+	$('.background-1').velocity("fadeIn", { duration: 6000 });
+	$('.background-2').velocity("fadeOut", { duration: 8000 });
+};
+// fade in bg2 and fade out bg1
+var fade2 = function(){
+	$('.background-2').velocity("fadeIn", { duration: 6000 });
+	$('.background-1').velocity("fadeOut", { duration: 8000 });
+};
+
+// timeout function to create new svg every 10 seconds
+var recreateSvg = function(){
+ 	window.setInterval(svgNew, 10000);
+};
+
+// redraw the current svg to match screen size on resize
+$(window).resize(function() {
+	svg.width = $(window).width();
+	svg.height = $(window).height();
+	$('#bg-container').css({
+		'min-width': svg.width,
+		'min-height': svg.height
+	});
+	svgDraw1('resize');
+	svgDraw2('resize');
+});
